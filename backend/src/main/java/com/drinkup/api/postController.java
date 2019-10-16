132737b554754a55c8442f30d3178a4e2535e062
Ryan Rohm <rjr61@pitt.edu>
2019-10-15 21:52:10 -0400

    fixed more fuckups

--- backend/src/main/java/com/drinkup/api/postController.java
+++ backend/src/main/java/com/drinkup/api/postController.java
@@ -1,6 +1,5 @@
 package com.drinkup.api;
 import java.util.List;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
