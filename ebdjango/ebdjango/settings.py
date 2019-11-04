3bf265c46c0c205f196c267ecf451c0e3b0de4ae
crkolimago <crkolimago@gmail.com>
2019-11-04 12:33:45 -0500

    11

--- ebdjango/ebdjango/settings.py
+++ ebdjango/ebdjango/settings.py
@@ -142,9 +142,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'ebdjango/static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ebdjango/static'), 
-]
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'ebdjango/static'), 
+#]
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
