ad85ff444598f06df5cb4ea57ce87d9dd6e9b0d6
Ryan Rohm <ryanrohm33@gmail.com>
2019-11-04 18:51:38 -0500

    fucking tests

--- ebdjango/profile/tests.py
+++ ebdjango/profile/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+
+class AccountTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        self.selenium = webdriver.Firefox()
+        super(AccountTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(AccountTestCase, self).tearDown()
+
+    def test_register(self):
+        selenium = self.selenium
+        #Opening the link we want to test
+        selenium.get('http://ebdjango-dev2222.us-east-1.elasticbeanstalk.com/login/?next=/')
+        #find the form element
+        username = selenium.find_element_by_id('id_username')
+        password = selenium.find_element_by_id('id_password')
+
+        submit = selenium.find_element_by_name('login-button')
+
+        #Fill the form with data
+        username.send_keys('ryan')
+        password.send_keys('fantaorange')
+
+        #submitting the form
+        submit.send_keys(Keys.RETURN)
+
+        #check the returned result
+        assert 'http://ebdjango-dev2222.us-east-1.elasticbeanstalk.com/' in selenium.current_url
