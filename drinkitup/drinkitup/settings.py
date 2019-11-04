1ca8db2fe83ca097a0a61b72c0c6965dbe66f686
crkolimago <crkolimago@gmail.com>
2019-11-03 22:19:08 -0500

    bout to make some name changes

--- drinkitup/drinkitup/settings.py
+++ drinkitup/drinkitup/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'django-env.ji8ccjisxe.us-east-1.elasticbeanstalk.com',
+]
 
 
 # Application definition
