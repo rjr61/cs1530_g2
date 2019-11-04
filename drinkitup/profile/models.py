6d81cabd41cb93ebb92fb5659a9f72c12c5c534f
crkolimago <crkolimago@gmail.com>
2019-11-03 22:45:22 -0500

    deleting old files

--- drinkitup/profile/models.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-from django.db import models
-from django.utils import timezone
-
-# class User(models.Model):
-
-#     def __str__(self):
-#         return self.user_name
--- drinkitup/profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.decorators import login_required
-
-from . import views
-
-app_name = 'profile'
-urlpatterns = [
-    path('',  login_required(views.index, login_url='login/'), name='index'),
-]
