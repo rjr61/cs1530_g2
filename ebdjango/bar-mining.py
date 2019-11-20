a3623c8062077299ed06707522879f0eec650809
crkolimago <crkolimago@gmail.com>
2019-11-20 12:35:59 -0500

    got bar mining working

--- ebdjango/bar-mining.py
+++ ebdjango/bar-mining.py
@@ -1,24 +1,9 @@
-## First search using:
-# https://plcbplus.pa.gov/pub/Default.aspx?PossePresentation=LicenseSearch
-# License-Type: All
-# License-Status: Active
-# County: Allegheny
-# Zip Code: 15213
-
-## Second download the page source to `license-data.txt`
-
 from bs4 import BeautifulSoup
-import re, json
+import requests
 
-f = open("license-data.txt", "r")
-contents = f.read()
-soup = BeautifulSoup(contents, 'html.parser')
-name_anchors = soup.find_all(id=re.compile("Premise_"))
-names = [anchor.find("b").contents[0] for anchor in name_anchors]
+html = requests.get("http://www.dotheburgh.com/bars/").text
+soup = BeautifulSoup(html, 'html.parser')
+title_anchors = soup.find_all("a", class_="title")
+bar_names = [anchor.contents[0] for anchor in title_anchors]
 
-print(names)
-
-# with open('all_locations.json', 'w', encoding='utf8') as outfile:
-#    json.dump(names, outfile)
-
-## Third run using `python bar-mining.py`
+print(bar_names)
--- ebdjango/posts/static/posts/local_bars.js
+++ ebdjango/posts/static/posts/local_bars.js
@@ -20,29 +20,7 @@ var substringMatcher = function(strs) {
     };
   };
   
-  var states = ['TTMS INC', "HEMINGWAY'S CAFE", 'ORIGINAL HOT DOG SHOP', 
-  'MAGEE-WOMENS HOSPITAL', 'MELLINGER BEER DISTRIBUTOR, INC.  ', 
-  'GATEWAY WINE & SPIRITS, INC.', 'MAD MEX', 'ANDREA CORTESE-HASSETT,PHD ITX M DIAGNOSTICS', 
-  'UNION GRILL', 'THE TWENTIETH CENTURY CLUB', 'PRINCE OF INDIA RESTAURANT & TAVERN, INC.', 
-  'AMERICANA BAR CORPORATION', 'PIV-CON, INC.', "HIEBER'S PHARMACY, INC.", 
-  'SCHENLEY CENTER ASSOCIATES, L.P. & CROSSROADS HOSPITALITY CO., L.L.C.', 
-  'CARNEGIE MUSEUM OF NATURAL HISTORY', 'KOREA GARDEN', "GENE'S PLACE", 
-  'PHIPPS CONSERVATORY AND BOTANICAL GARDENS INC', 'THE SPICE ISLAND TEA HOUSE', 
-  'SOUTH SIDE SIN CITY INC', 'P TOWN', 'CRAIG DISTRIBUTING COMPANY', 
-  'UNIVERSITY OF PITTSBURGH OF THE COMMONWEALTH SYSTEM OF HIGHER ED', 
-  'SOLDIERS & SAILORS MEMORIAL HALL & MUSEUM TRUST INC', 'LEGUME INC', 
-  'BLACK BEAN CUBAN CUISINE', "PITT'S DOGG'N IT LLC", 
-  'URGO HOTELS LP AND RLJ III - HGN PITTSBURGH LESSEE LP', 
-  "EAT'N PARK HOSPITALITY GROUP INC", 'ALI-BABA', 'WYNDHAM PITTSBURGH UNIVERSITY CENTER', 
-  'CAMPUS BOOKSTORE INC', 'CREPES PARISIENNES LLC', 'KBOX KARAOKE HOUSE', 
-  'UPMC PRESBYTERIAN SHADYSIDE PRESBYTERIAN CAMPUS', 'PAPA DA VINCI LLC', 'PRIMANTI BROS', 
-  'MERO OAKLAND INC', 'CARNEGIE MUSEUM OF ART & NAT HIST', 
-  'NEXT CHAPTER WINES & SPIRITS LLC', 'THE CRICKET LOUNGE', 
-  'GIANT EAGLE INC', "MITCHELL'S TAVERN & RESTAURANT", 'CARNEGIE MELLON UNIVERSITY', 
-  'KHALILS RESTAURANT', "UNCLE JIMMY'S", 'THE GREEK COMMUNITY OF ALLEGHENY COUNTY PA', 
-  'CARLOW UNIVERSITY', '5TH YEAR INC', 'MERO CHIKN INC', 'THE OAKLANDER', 
-  'EMILIANOS CENTRE AVENUE LLC', 'LUCCA RISTORANTE', 'CARNEGIE MELLON UNIVERSITY', 
-  "ZARRA'S"];
+  var bars = ['1311', '1947 Tavern', '20th Street Bar & Grill (New Feb 2015)', '5801 Video Lounge & Cafe', 'Acacia', 'Alexander’s', 'Antoon’s Pizza', 'Apple Inn', 'Archie’s Bar', 'Arsenal Cider House', 'Arsenal Lanes', 'August Henry’s City Saloon', 'AVA Lounge', 'Bangkok Balcony', 'Bar 11', 'Beehive Coffeehouse', 'Belvedere’s', 'Bigham Tavern', 'Birmingham Bridge Tavern', 'Bites and Brews', 'Bloomfield Bridge Tavern', 'Blue Moon', 'Bootleggers', 'Brewski’s', 'BRGR', 'Brillobox', 'Buckhead Saloon', 'Buffalo Blues', 'Butterjoint', 'Cafe’ Niko’s', 'Caliente Pizza and Bar', 'Cappy’s Cafe', 'Capri Pizzeria', 'Carmella’s Plates & Pints', 'Carson Street Deli & Craft Beer Bar', 'Casbah', 'Cattivo', 'Cheesecake Factory', 'Church Brew Works', 'Corner Cafe', 'Cupka’s Cafe 2', 'D’s Six Pax & Dogz', 'Dee’s Cafe', 'Del’s Bar & Restaurant', 'Devils and Dolls', 'Double Wide Grill', 'Dunning’s Grill', 'Easy Street', 'Eclipse Lounge', 'Eleven Contemporary Kitchen', 'Emilianos Mexican Restaurant', 'Excuses Bar and Grill', 'Fuel & Fuddle', 'Garage Door Saloon', 'Gene’s Place', 'Gorman’s Pub', 'Graziano’s Pizzeria', 'Hambone’s Pub', 'Hard Rock Cafe', 'Harris Grill', 'Hemingway’s Cafe', 'Hofbräuhaus Pittsburgh', 'Hot Rod Cafe', 'Hough’s Taproom & BrewPub', 'Howlers Coyote Cafe', 'Images Bar', 'Industry Public House', 'Jack’s Bar', 'Jekyl & Hyde', 'Jimmy D’s', 'Joe & Pie', 'Kelly’s Bar and Lounge', 'Lava Lounge', 'Le Mardi Gras', 'local bar + kitchen', 'Logan’s Pub', 'Lombardozzi’s Restaurant', 'Lot 17 Bar & Grill', 'Lou’s Little Corner Bar', 'Mad Mex', 'Mad Mex', 'Mario’s East Side Saloon', 'Mario’s South Side Saloon', 'Micro Diner', 'Milkshake Factory', 'Mineo’s Pizza House', 'Mint Hookah Bar & Lounge', 'Mitchell’s Tavern', 'Murphy’s Tap Room', 'Murray Avenue Grill', 'Nadine’s Bar & Restaurant', 'Nakama', 'Napoli’s Pizzeria', 'New Amsterdam', 'New Dumpling House', 'Nico’s Recovery Room', 'Nied’s Hotel', 'Nola on the Square', 'Olio Trattoria', 'Over The Bar Bicycle Cafe', 'Packs & Dogs', 'Papa DaVinci', 'Peter’s Pub', 'Piper’s Pub', 'Pizza Bellino', 'Pizza Parma', 'Pizza Romano', 'Pizza Sola', 'Pleasure Bar & Restaurant', 'Poppy’s Bistro & Pub', 'Potatoheads Bar and Grill', 'Pregame Bar', 'Primanti Bros.', 'Primanti Bros. South Side', 'Prince of India', 'Pusadee’s Garden', 'Quaker Steak & Lube', 'Redbeard’s Bar & Grill', 'Remedy', 'Round Corner Cantina', 'Ruggers Pub', 'Rusty Barrel Saloon', 'Ryan’s Pub & Grill', 'S. Aiken Bar and Grille', 'Sakura Teppanyaki & Sushi', 'Salt of the Earth', 'Satalio’s', 'Scarpaci’s', 'Shady Grove', 'Shadyside Hideaway Bar', 'Shiloh Grill', 'Silky’s Olde Thyme Pub', 'Sloppy Joe’s', 'Smokin’ Joe’s Saloon', 'SOBA Lounge', 'Social at Bakery Square', 'Sonny’s Tavern', 'Sonoma Grille', 'Sorrento’s Pizza Roma', 'South Side BBQ Company', 'Sphinx Cafe', 'Spin Bartini & Ultra Lounge', 'Spoon', 'Squirrel Hill Cafe', 'Stack’d Burgers & Beer', 'Steel Cactus', 'Stinky’s Bar and Grill', 'Streets on Carson', 'Sushi Too', 'Take A Break', 'Taste Of India', 'Tavern 245', 'Tender Bar + Kitchen', 'Tessaro’s', 'Thai Place', 'The Allegheny Wine Mixer', 'The Library', 'The Map Room', 'The Original Hot Dog Shop', 'The Rowdy Buck', 'The Smiling Moose', 'The South Paw', 'The Summit', 'The Urban Tap', 'The Yard', 'Thunderbird Cafe', 'Tiki Lounge', 'Toro’s Tavern', 'Trapuzzano Italian Chophouse', 'Twelve on Carson', 'Uncle Jimmy’s Tavern', 'Union Grill', 'Vocelli Pizza', 'William Penn Tavern', 'XO Cafe & Lounge'];
   
   $('#the-basics .typeahead').typeahead({
     hint: true,
@@ -50,6 +28,6 @@ var substringMatcher = function(strs) {
     minLength: 1
   },
   {
-    name: 'states',
-    source: substringMatcher(states)
+    name: 'bars',
+    source: substringMatcher(bars)
   });
